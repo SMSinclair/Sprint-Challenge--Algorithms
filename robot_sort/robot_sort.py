2fe6d6634bb72d57af3801efddc36cd6c4d575b9
Stephen Sinclair <smsinclair@gmail.com>
2019-11-08 12:28:16 -0500

    robot test passed

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,17 +96,36 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        
+        self.set_light_on()
+        while(self.light_is_on()):
+            self.set_light_off() #light off, no swaps yet
+            while(self.can_move_left()):
+                self.move_left()
 
-
+            while(self.can_move_right()):
+                self.swap_item() # pick up item 1
+                #print(f"picked up {self._item}")
+                self.move_right() # move right
+                #print("move right")
+                if(self.compare_item()==1): #compare
+                    self.set_light_on() # if item 2 is greater swap and turn light on
+                    self.swap_item() 
+                    #print(f"swapped with {self._item}, light is {self._light}")
+               
+                self.move_left() # reset for next iteration
+                self.swap_item()
+                #print(f"moved to {self._position} and swapped for {self._item}")
+                self.move_right()
+                #print(f"moved right to {self._position}")
+    
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
-    robot = SortingRobot(l)
+    j = [4,3,2,1]
+    robot = SortingRobot(j)
 
     robot.sort()
     print(robot._list)
